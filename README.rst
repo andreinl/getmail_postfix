9c57e146f743459d1ac4dc787da09bccff1a6290
root <root@simec.int>
2017-01-19 09:09:39 +0000

    Initial commit

---
+++ README.rst
@@ -0,0 +1,54 @@
+Postfix - Getmail connector
+===========================
+
+This program permits to use PostfixAdmin (http://postfixadmin.sourceforge.net/) to create Getmail (http://pyropus.ca/software/getmail/) configuration files.
+When program is executed it compares modification time of getmailrc file and modification time of the record in database.
+If getmailrc file is outdated it will be recreated with new values.
+
+
+Installation
+============
+* Install required modules (In this example PostfixAdmin uses MySQL database.):
+
+    pip install pony
+    pip install jinja2
+    pip install PyMySQL
+
+..note:: Script is tested with **Python 3.4** but it should work with **Python 2.6+** also.
+         In case both of Python versions are installed you probably need to use pip3 instead of pip
+
+* Download (clone) this files.
+* Create config.py. It should look like this::
+
+    from pony import orm
+
+    debug = False
+
+    host = 'localhost'
+    db = 'postfix'
+    user = 'postfixadmin'
+    passwd = '<postfixadmin password>'
+
+    tmpl_dir = '/opt/postfix_getmail'
+    rc_path = '/etc/getmail'
+    base_path = '/var/vmail/'
+
+    database = orm.Database('mysql', host=host, user=user, passwd=passwd, db=db)
+
+
+In this example Getmail rc files are in */etc/getmail* and emails are in */var/vmail/*.
+Program and all its files are in */opt/postfix_getmail*
+
+* create /etc/cron.10min and add in /etc/crontab this line:
+
+    */10 *  * * *   root    cd / && run-parts --report /etc/cron.10min
+
+* create soft link to getmail_postfix.py:
+
+    cd /etc/cron.10min
+    ln -s /opt/postfix_getmail/getmail_postfix.py getmail_postfix
+
+* make getmail_postfix.py executable:
+
+    chmod +x /opt/postfix_getmail/getmail_postfix.py
+
---
+++ getmail_postfix.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+#
+# Requirements:
+#    pony
+#    PyMySQL
+#    jinja2
+#
+
+
+from pony import orm
+from pony.orm import unicode
+from datetime import datetime
+from base64 import b64decode
+from jinja2 import Environment, FileSystemLoader
+import os
+from config import debug, tmpl_dir, rc_path, database
+import subprocess
+import logging
+import sys
+
+
+FORMAT = "%(asctime)-15s %(user)-8s %(message)s"
+logging.basicConfig(format=FORMAT)
+
+logger = logging.getLogger('getmail_postfix')
+
+if debug:
+    logger.setLevel(logging.DEBUG)
+
+
+class Fetchmail(database.Entity):
+    """
+    Pony ORM model of the Fetchmail table
+    """
+    domain = orm.Required(unicode)
+    mailbox = orm.Required(unicode)
+    src_server = orm.Required(unicode)
+    src_user = orm.Required(unicode)
+    src_password = orm.Required(unicode)
+    usessl = orm.Optional(bool)
+    fetchall = orm.Optional(bool)
+    keep = orm.Optional(bool)
+    modified = orm.Required(datetime)
+    active = orm.Required(bool)
+
+
+def get_getmailrc(mailbox_user, mailbox):
+    j2_env = Environment(loader=FileSystemLoader(tmpl_dir),
+                         trim_blocks=True)
+
+    return j2_env.get_template('getmailrc.tmpl').render(
+        box=box,
+        context={
+            'multi': False,
+            'base_path': base_path,
+            'mailbox_user': mailbox_user,
+            'src_password': b64decode(mailbox.src_password).decode('utf-8'),
+            'rc_path': rc_path.rstrip('/')
+        }
+    )
+
+
+def is_running():
+    proc = subprocess.Popen(
+        'ps ax | grep /usr/bin/getmail | grep -c --invert-match grep',
+        shell=True,
+        stdout=subprocess.PIPE
+    )
+    count = proc.stdout.readline()
+    return count.decode('utf-8').strip() != '0'
+
+
+if __name__ == "__main__":
+    if debug:
+        # turn on debug mode
+        orm.sql_debug(True)
+
+    if not os.path.isdir(rc_path):
+        os.mkdir(rc_path)
+
+    if os.path.isfile(os.path.join(rc_path, 'stop')):
+        sys.exit()
+
+    if is_running():
+        logger.debug("Getmail is running, exiting now", extra={'user': '-'})
+        sys.exit()
+
+    # use db_session as a context manager
+    with orm.db_session:
+        # map the models to the database
+        database.generate_mapping(create_tables=False)
+
+        mailboxes = orm.select(mailbox for mailbox in Fetchmail if mailbox.active)
+        for box in mailboxes:
+            mailbox_user = box.mailbox.split('@')[0]
+
+            context = {
+                'user': mailbox_user
+            }
+
+            rc_file_name = 'getmailrc.{mailbox_user}{box.id}'.format(mailbox_user=mailbox_user, box=box)
+            rc_file = os.path.join(rc_path, rc_file_name)
+
+            if not os.path.isfile(rc_file) or datetime.fromtimestamp(os.path.getmtime(rc_file)) < box.modified:
+                with open(rc_file, 'w') as new_getmailrc:
+                    logger.debug('Creating new rc file for %s ...', mailbox_user, extra=context)
+                    new_getmailrc.write(get_getmailrc(mailbox_user, box))
+            else:
+                logger.debug("rc file for '%s' user is not changed", mailbox_user, extra=context)
+
+            subprocess.call(['/usr/bin/getmail', '-g', rc_path, '-r', rc_file_name])
